a9159f2eef5058d832dceec8991f7a14f61cbb0a
Alice Wu <110137320+alicewu1@users.noreply.github.com>
2022-09-05 16:30:01 -0400

    Edited py file

--- scripts/datacleaning.py
+++ scripts/datacleaning.py
@@ -10,7 +10,8 @@ df ##view output
 df.shape # count dimensions: rows by columns 
 df ## view output: 741876 rows x 9 columns 
 
-# READING COLUMN AND ROW NAMES
+
+### READING COLUMN AND ROW NAMES ###
 df.head() # reads first 5 column names
 column_names = df.columns # define variable 
 column_names # view column names 
@@ -18,16 +19,16 @@ row_names = df.index # define variable
 row_names #reads row names 
 
 
-# CLEANING COLUMNS
+### CLEANING COLUMNS ###
 df.columns = df.columns.str.replace('[^A-Za-z0-9]+', '_') ## regex formatting for strings
 list(df)
 # replace all whitespace in column names with an underscore
 df.columns = df.columns.str.replace(' ', '_')
-df.columns
+df.columns # view output
 
-# COLUMN TYPES
+
+### COLUMN TYPES ###
 df.dtypes #view column types to see if strings are really strings, number are numbers, dates are dates, and boolean are booleans
-
 
 # create a list of columns that are strings, and save as strings 
 strings = df.select_dtypes(include=['object']).columns
@@ -39,3 +40,29 @@ dates = df.select_dtypes(include=['datetime64[ns]']).columns
 booleans = df.select_dtypes(include=['bool']).columns
 # create a list of columns that are objects, and save as objects
 objects = df.select_dtypes(include=['object']).columns
+
+
+### DUPLICATE ROWS ### 
+df.columns.duplicated() # look of duplicate rows 
+
+
+### REMOVING WHITESPACE ###
+# .sum tp get a count of missing values in each column
+df.isnull().sum() ## Identified Student Column with missing 604 values
+
+# replacing blank, empty cells with NaN 
+#NaN tells python that it is a missing value instead of white space that we can't see
+df.replace(to_replace='', value=np.nan, inplace=True)
+
+# drop rows with missing values
+df.dropna(inplace=True) # drop rows with missing values
+df.isnull().sum() #run again to double verify whitespace was removed
+
+
+### CONVERTING COLUMNS ###
+# Convert Week column from object to datetime
+df['Week'] = pd.to_datetime(df['Week'])
+
+#removed empty cells with NaaN before converting Student_Count column
+df['Student_Count'] = df['Student_Count'].astype(int)#convert Student_Count column from float64 to int
+df.dtypes # re-check datatypes
